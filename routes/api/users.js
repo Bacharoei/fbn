e9a3827c60e1f19513818b3ae523c160a41df81d
Roei Oz Bachar <roei.ob@zerto.com>
2020-08-30 14:30:23 +0300

    created auth routes and users auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route    Post api/users
 // @desc     Register User Route
@@ -33,7 +35,9 @@ router.post(
       //Check if the user exist
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -56,7 +60,21 @@ router.post(
       //save the user to DB
       await user.save();
 
-      res.send("users registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");

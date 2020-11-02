76c5f98a3d464e0df3de3e79312f0f772962a8d1
Roei Bachar <roeibachar1@gmail.com>
2020-11-02 09:58:32 +0200

    created login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import Landing from './components/layout/Landing';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import dashboard from './components/layout/dashboard';
 
 
 const App = () => 
@@ -10,10 +15,14 @@ const App = () =>
 <Router>
     <>
     <Navbar />
-    <Landing />
-  
-
-
+    <Route exact path="/" component={Landing} />
+    <section className="container">
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="dashboard" component={dashboard} />
+      </Switch>
+    </section>
   </>
 </Router>
 
